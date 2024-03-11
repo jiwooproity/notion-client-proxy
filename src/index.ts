1f690d7761732b2c8854273445c07470fea79324
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-03-11 22:37:18 +0900

    Feat: 메모 남기기 API 기능 추가

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
 import { Request, Response } from "express";
 
-const { getMemoList } = require("./memo/client");
+const { getMemoList, createMemo } = require("./memo/client");
 const express = require("express");
 const cors = require("cors");
 
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/memo-list", async (req: Request, res: Response) => {
   const data = await getMemoList();
   res.json(data);
+});
+
+app.post("/memo-create", async (req: Request, res: Response) => {
+  const { title, content } = req.body;
+  await createMemo({ title, content });
+  res.send({ status: 200 });
 });
 
 app.listen(port, console.log(`Server started on PORT: ${port}`));
