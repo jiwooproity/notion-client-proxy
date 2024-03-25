f51eceb2993ba7e7c0ce7da6b050d1f0397bcd8b
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-03-25 19:33:01 +0900

    Feat: Socket.io 적용 및 메모 알림 기능 구현

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { Socket } from "socket.io";
 
 const { getMemoList, createMemo } = require("./memo/client");
 const express = require("express");
@@ -9,6 +10,11 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: { origin: "*", methods: ["GET", "POST"] },
+});
 
 app.get("/memo", async (req: Request, res: Response) => {
   const data = await getMemoList();
@@ -21,4 +27,10 @@ app.post("/memo", async (req: Request, res: Response) => {
   res.send({ status: 200 });
 });
 
-app.listen(port, console.log(`Server started on PORT: ${port}`));
+io.on("connection", (socket: Socket) => {
+  socket.on("insert", () => {
+    socket.broadcast.emit("alert");
+  });
+});
+
+server.listen(port, console.log(`Server started on PORT: ${port}`));
