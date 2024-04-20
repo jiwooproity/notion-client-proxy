c795c9db0f3e5dae4705ada9c9713f6ba03593b5
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-04-20 21:39:04 +0900

    Feat: Image, Audio File Stream API 추가 - 각 파일에 대한 API URI 추가 - 오디오, 이미지 파일 접근 가능 - 뮤직 플레이어 애플리케이션 개발을 위한 API 기능

---
+++ src/music/index.ts
@@ -0,0 +1,41 @@
+import { readdirSync, statSync, createReadStream } from "fs";
+import path from "path";
+
+const audioBasePath = "../storage/audio";
+
+const syncFile = (filename: string) => {
+  const filePath = path.resolve(__dirname, `${audioBasePath}/${filename}`);
+  const fileSync = statSync(filePath);
+  return { path: filePath, size: fileSync.size };
+};
+
+const syncDir = () => {
+  const filePath = path.resolve(__dirname, audioBasePath);
+  return readdirSync(filePath);
+};
+
+const getMusic = (filename: string) => {
+  const { path, size } = syncFile(`${filename}.mp3`);
+  return { stream: createReadStream(path), size };
+};
+
+const getMusicList = () => {
+  const audioDir = syncDir();
+
+  const getAudioInfo = (file: string) => {
+    const [filename, type] = file.split(".");
+    const isProd = process.env.NODE_ENV == "production";
+    const url = isProd ? "https://www.jiwoo.so" : "http://localhost:8080";
+
+    return {
+      title: filename,
+      filename,
+      cover: `${url}/image/${filename}`.replace(/(\s)/g, "%20"),
+      type,
+    };
+  };
+
+  return audioDir.map(getAudioInfo);
+};
+
+export { getMusic, getMusicList };
