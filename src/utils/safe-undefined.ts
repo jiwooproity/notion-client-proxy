2bdfca2de43fd868ba0c89792d50b268e0321032
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-06-10 17:18:04 +0900

    Feat: Memo 리스트 reaction 필드 추가 - Notion DB Reaction 필드 데이터 추가 - 객체 접근 시, undefined 처리 및 default 반환 함수 추가

---
+++ src/utils/safe-undefined.ts
@@ -0,0 +1,13 @@
+const safeUndefined = <T, R>(origin: T, target: string, tobe: string): R => {
+  const accessPath = target.split(".");
+  let current = origin as any;
+
+  for (const key of accessPath) {
+    if (current === undefined) return tobe as R;
+    current = current[key];
+  }
+
+  return current as R;
+};
+
+export default safeUndefined;
